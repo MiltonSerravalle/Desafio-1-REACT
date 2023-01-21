97cdac318c4187e7a339922e1b21fd6d4fe25f9b
Milton Serravalle <milton.serravalle@hotmail.com>
2023-01-21 20:08:41 -0300

    2do commit

--- desafio/src/NavBar/NavBar.jsx
+++ desafio/src/NavBar/NavBar.jsx
@@ -1,19 +1,21 @@
-import CartWidget from "../CartWidget/CartWidget"
-import "./NavBar.css"
+import CartWidget from "../CartWidget/CartWidget";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <div className="navDiv">
       <h1 className="navTitle">San Justo Sneakers</h1>
-      <ul className="navList">
-        <li className="listItem">Inicio</li>
-        <li className="listItem">Productos</li>
-        <li className="listItem">Contacto</li>
-        <li className="listItem">Quienes Somos?</li>
-      </ul>
-      <CartWidget />
+      <div className="navListCart">
+        <ul className="navList">
+          <li className="listItem">Inicio</li>
+          <li className="listItem">Productos</li>
+          <li className="listItem">Contacto</li>
+          <li className="listItem">Quienes Somos?</li>
+        </ul>
+        <CartWidget />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
