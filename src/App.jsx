32d833111e8cb98dc0358c03d4374ff16d68ee43
Milton Serravalle <milton.serravalle@hotmail.com>
2023-02-02 20:05:12 -0300

    routes and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ItemCount from "./components/ItemCount/ItemCount";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
 
 function App(props) {
   return (
+  <BrowserRouter>
     <div className="appBody">
       <NavBar />
-      <ItemListContainer />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/detalle" element={<ItemDetailContainer /> } />      
+      </Routes>
       <ItemCount />
-      <ItemDetailContainer />
     </div>
+    </BrowserRouter>
   );
 }
 
