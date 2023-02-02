9fdcede84f5d555126939e04cf196e19fc707cd9
Milton Serravalle <milton.serravalle@hotmail.com>
2023-02-01 22:00:44 -0300

    falta css pero renderiza

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,28 +2,37 @@ import { useEffect, useState } from "react";
 import "./ItemListContainer.css";
 import { gFetch } from "../../utils/gFetch";
 
-
 export const ItemListContainer = ({ greeting }) => {
- const [ productos, setProductos ]  = useState([])
- const [ loading, setLoading ] = useState(true)
+  const [productos, setProductos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
- console.log(productos)
-return(
-  <center>
-{loading ? <h3>-- Cargando --</h3> 
-:
-productos.map(producto => <div key={producto.id}>
-  <div className= 'cardHeader'>
-    Nombre : {producto.name} 
-    </div>
-    <div className="cardBody"> 
-    <img src={producto.foto}></img>
-    Categoria: {producto.category} <br />
-    Precio: {producto.price}
-    </div>
-</div>)}
+  useEffect(() => {
+    gFetch()
+      .then((res) => {
+        setProductos(res);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
 
-  </center>
-)
-}
+  console.log(productos);
+  return (
+    <center>
+      {loading ? (
+        <h3>-- Cargando --</h3>
+      ) : (
+        productos.map((producto) => (
+          <div className="card" key={producto.id}>
+            <div className="cardHeader">Nombre : {producto.name}</div>
+            <div className="cardBody">
+              <img className="cardImg" src={producto.foto}></img>
+              Categoria: {producto.category} <br />
+              Precio: {producto.price}
+            </div>
+          </div>
+        ))
+      )}
+    </center>
+  );
+};
 export default ItemListContainer;
