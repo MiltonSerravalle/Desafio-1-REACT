01701d52534124cacff41825be7af8df6cc9b78f
Milton Serravalle <milton.serravalle@hotmail.com>
2023-02-24 18:47:51 -0300

    Intercambiabilidad ItemCount

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,35 +1,51 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, NavLink } from "react-router-dom";
 
+const ItemCount = ({}) => {
+  const [count, setCount] = useState(0);
+  const [inputType, setInputType] = useState("buttons");
 
-const ItemCount = ({addToCart}) => {
-const [count, setCount] = useState(0); 
+  const handleClick = (value) => {
+    if (count + value >= 0) {
+      setCount(count + value);
+    }
+  };
 
-const handleClick = (value) => {
-  if (count + value >= 0) {
-    setCount(count + value);
-  }
-}
-
-const handleAddToCart = () => {
-  addToCart(count);
-  setCount(0);
-}
+  const handleAddToCart = () => {
+    setInputType('links')
+  };
 
   return (
     <div className="d-flex mt-3">
-      <button className="btn btn-danger" onClick={() => handleClick(-1)}>
-        -
-      </button>
-      <p className="m-0 font-weight-bold" style={{ fontSize: "1.5em", width: "50px", textAlign: "center" }}>{count}</p>
-      <button className="btn btn-success" onClick={() => handleClick(1)}>
-        +
-      </button>
-      <button className="btn btn-dark ml-3" onClick={handleAddToCart}>
-        Agregar al carrito
-      </button>
+      {inputType === "buttons" ? (
+        <>
+          <button className="btn btn-danger" onClick={() => handleClick(-1)}>
+            -
+          </button>
+          <p
+            className="m-0 font-weight-bold"
+            style={{ fontSize: "1.5em", width: "50px", textAlign: "center" }}
+          >
+            {count}
+          </p>
+          <button className="btn btn-success" onClick={() => handleClick(1)}>
+            +
+          </button>
+          <button className="btn btn-dark ml-3" onClick={handleAddToCart}>
+            Agregar al carrito
+          </button>
+        </>
+      ) : (
+        <>
+          <Link to="/cart">
+            <button className="btn btn-dark ml-3"> Mi Carrito </button>
+          </Link>
+          <Link to="/" className="btn btn-dark ml-3"> Seguir comprando </Link>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ItemCount
+export default ItemCount;
