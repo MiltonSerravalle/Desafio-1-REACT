f8537edfc6438308f2801e870725ac123c2088c1
Milton Serravalle <milton.serravalle@hotmail.com>
2023-02-23 22:46:42 -0300

    contador

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
-const ItemCount = () => {
+const ItemCount = ({addToCart}) => {
+const [count, setCount] = useState(0); 
+
+const handleClick = (value) => {
+  if (count + value >= 0) {
+    setCount(count + value);
+  }
+}
+
+const handleAddToCart = () => {
+  addToCart(count);
+  setCount(0);
+}
+
   return (
-    <div></div>
+    <div className="d-flex mt-3">
+      <button className="btn btn-danger" onClick={() => handleClick(-1)}>
+        -
+      </button>
+      <p className="m-0 font-weight-bold" style={{ fontSize: "1.5em", width: "50px", textAlign: "center" }}>{count}</p>
+      <button className="btn btn-success" onClick={() => handleClick(1)}>
+        +
+      </button>
+      <button className="btn btn-dark ml-3" onClick={handleAddToCart}>
+        Agregar al carrito
+      </button>
+    </div>
   )
 }
 
