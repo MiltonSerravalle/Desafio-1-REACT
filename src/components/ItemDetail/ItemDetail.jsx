2e78f795e166d937933cd1b1df0669e6b0607dc0
Milton Serravalle <milton.serravalle@hotmail.com>
2023-02-27 22:10:35 -0300

    commit emprolijado cards

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -17,16 +17,16 @@ const ItemDetail = ({ productos }) => {
     return (
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <div className="card mx-2 mb-4" style={{width: "300px"}}>
+          <div className="card mx-2 mt-5 mb-3 text-bg-secondary border-secondary" style={{width: "300px"}}>
             <div className='card-header text-center'>
-              {productos.name}
+              <h3>{productos.name}</h3>
             </div>
             <div className='card-body text-center'>
               <img src={productos.foto} alt='imagen' className='product-image w-100'/>
             </div>
             
             <div className='card-body text-center'>
-              <strong>ARS ${productos.precio}</strong>
+              <h5><strong>ARS ${productos.precio}</strong></h5>
             </div>
             <div className='mx-auto m-3'>
             <ItemCount onAdd={onAdd} />
